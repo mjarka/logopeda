b669fef732f5341b303ee66e70c58529bda0fbb1
Marcin <marcinjarka+heroku@gmail.com>
2022-06-28 08:09:00 +0200

    Routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { BrowserRouter, HashRouter } from 'react-router-dom';
-import Routes from './Routes';
-import Page from './components/Page';
 
+import Page from './components/Page';
+import { Routes as ReactRoutes, Route, Navigate } from 'react-router-dom';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'react-image-lightbox/style.css';
 import 'aos/dist/aos.css';
-
+import Coworking from 'views/Coworking/Coworking';
 const App = () => {
   return (
     <Page>
       <HashRouter>
-        <Routes />
+        {/* <Routes /> */}
+        <ReactRoutes>
+          <Route path="/" element={<Coworking />}></Route>
+        </ReactRoutes>
       </HashRouter>
     </Page>
   );
